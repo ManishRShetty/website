c8f318cf38c85756a9028bbb86136a792ff8dc97
ManishRShetty <143414001+ManishRShetty@users.noreply.github.com>
2024-08-28 11:36:21 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,35 @@
+# def calc():
+#     a=int(input("Enter the Value of a "))
+#     b=int(input("Enter the Value of b "))
+#     print(f"Addition of {a} and {b} is {a+b}")
+#     print(f"Subtraction of {a} and {b} is {a-b}")
+#     print(f"Division of {a} and {b} is {a/b}")
+#     print(f"Multiplication of {a} and {b} is {a*b}")
+
+# def comp(a,b):
+#     if(a>b):
+#         print(f"{a} is greater then {b}")
+
+# try:
+#     calc()
+# except:
+#     print(TypeError)
+
+
+# s_user="User123"
+# s_pass="User@123"
+
+# u_name=input("Enter username")
+# u_pass=input("Enter the passsword")
+
+# if s_user==u_name and s_pass==u_pass:
+#     print("Login succefull")
+# else:
+#     print("Access invalid")
+
+
+num=107;
+if num%5==0:
+    print("Divisible")
+else:
+    print("No")
